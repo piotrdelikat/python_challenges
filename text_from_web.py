a397dd1bf86f6629596e69188b8a8299d026e748
Piotr Delikat <piotrdellikat@gmail.com>
2017-10-20 01:52:01 +0200

    structure, main()

--- text_from_web.py
+++ text_from_web.py
@@ -1,11 +1,22 @@
 #get text as strings from txt file at URL
+import sys
 from urllib.request import urlopen
 
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words = []
-    for line in story:
-        line_words = line.decode('utf-8').split()
-        for word in line_words:
-            story_words.append(word)
+def get_words(url):
+    with urlopen(url) as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
 
-print(story_words)
+def print_words(items):
+    for item in items:
+        print(item)
+
+def main(url):
+    words = get_words(url)
+    print_words(words)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
