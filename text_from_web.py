fd6ef38bd78ac713a88caba1ed232e0417fc8c46
Piotr Delikat <piotrdellikat@gmail.com>
2017-10-20 00:40:09 +0200

    txt to list of strings

---
+++ text_from_web.py
@@ -0,0 +1,11 @@
+#get text as strings from txt file at URL
+from urllib.request import urlopen
+
+with urlopen('http://sixty-north.com/c/t.txt') as story:
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+
+print(story_words)
