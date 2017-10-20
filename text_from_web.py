d255fb2df345eaeb69668fcbc435ccf62f0bfc70
Piotr Delikat <piotrdellikat@gmail.com>
2017-10-20 19:45:42 +0200

    docstrings, comments

--- text_from_web.py
+++ text_from_web.py
@@ -1,8 +1,19 @@
-#get text as strings from txt file at URL
+"""Retrieve and print words from a URL.
+
+Usage:
+
+    python text_from_web.py <URL>
+"""
+
 import sys
 from urllib.request import urlopen
 
 def get_words(url):
+    """Get a list of words from a URL.
+
+    :param url: The URL of a UTF-8 text document.
+    :returns A list of strings from the document.
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -11,12 +22,20 @@ def get_words(url):
                 story_words.append(word)
 
 def print_words(items):
+    """Print items one per line.
+
+    :param items: An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 def main(url):
+    """Print each word from a text document from a URL
+
+    :param url: The URL of a UTF-8 text document.
+    """
     words = get_words(url)
     print_words(words)
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1]) #The 0th arg is the module filename
