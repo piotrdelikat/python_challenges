694e022a004c57c860f747f9958abe1c634fb3d1
Piotr Delikat <piotrdellikat@gmail.com>
2017-11-13 19:54:03 +0100

    seats allocation method

--- classes.py
+++ classes.py
@@ -21,7 +21,7 @@ class Flight:
         self._aircraft = aircraft
 
         rows, seats = self._aircraft.seating_plan()
-        self._seating = [None] + [{letter: None for letter in seats} for _ in rows]
+        self._seating = [None] + [{letter:None for letter in seats} for _ in rows]
 
     def number(self):
         return self._number
@@ -32,6 +32,37 @@ class Flight:
     def aircraft_model(self):
         return self._aircraft.model()
 
+    def allocate_seat(self, seat, passenger):
+        """Allocate a seat to a passenger.
+
+        :arg
+            seat: A seat designator such as '12C' or '21F'.
+            passenger: The passenger name.
+
+        :raises
+            ValueError: If the seat is unavailable.
+        """
+
+        #assign a passanger name to the number in seating plan
+        rows, seat_letters = self._aircraft.seating_plan()
+
+        letter = seat[-1]
+        if letter not in seat_letters:
+            raise ValueError("Invalid seat letter {}".format(letter))
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row {}".format(row_text))
+
+        if row not in rows:
+            raise ValueError("Invalid row number {}".format(row))
+
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} already occupied".format(seat))
+
+        self._seating[row][letter] = passenger
 
 class Aircraft:
 
